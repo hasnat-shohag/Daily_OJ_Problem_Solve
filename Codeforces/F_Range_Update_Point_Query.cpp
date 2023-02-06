01dcb971b176cf9dc7e4c8ca3a4ed077947c8b29
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-02-06 22:25:30 +0600

    6 february 2023

---
+++ Codeforces/F_Range_Update_Point_Query.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0)
+#define TxtIO                         \
+    freopen("input.txt", "r", stdin); \
+    freopen("output.txt", "w", stdout);
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define f2(i, n) for (int i = 1; i < n; i++)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define int long long
+#define INF 1000000000
+#define mod 1000000007
+int Ceil(int a, int b)
+{
+    return (a + b - 1) / b;
+}
+//_________________template______________
+
+template <typename T> // printByVectorName
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    for (auto e : v)
+    {
+        os << e << " ";
+    }
+    return os;
+}
+
+void sol()
+{
+    int n;
+    cin >> n;
+    int q;
+    cin >> q;
+    vi v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    set<int> st;
+    vi m(n,0);
+    for (int i = 0; i < n; i++)
+    {
+        st.insert(i);
+    }
+
+    while (q--)
+    {
+        int a;
+        cin >> a;
+        if (a == 1)
+        {
+            int l, r;
+            cin >> l >> r;
+            --l,--r;
+            auto it = st.lower_bound(l);
+            if(it == st.end()){
+                continue;
+            }
+            int idx = *it;
+            for(int i = idx; i<= r;){
+                int tmp;
+                m[i]++;
+                tmp = v[i];
+                int sum = 0;
+                while (tmp > 0)
+                {
+                    int p = tmp % 10;
+                    sum += p;
+                    tmp /= 10;
+                }
+                v[i] = sum;
+                if(m[i] > 2){
+                    st.erase(i);
+                }
+                it = st.lower_bound(i+1);
+                
+                if(it != st.end()){
+                    i = *it;
+                }
+                else{
+                    break;
+                }
+            }
+        }
+        else
+        {
+            int x;
+            cin >> x;
+            cout << v[x - 1] << endl;
+        }
+    }
+}
+
+int32_t main()
+{
+    FastIO;
+    // TxtIO;
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        sol();
+    }
+}
