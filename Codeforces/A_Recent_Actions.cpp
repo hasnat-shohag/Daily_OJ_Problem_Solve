53db9eea80673fb77f73f09d80c06e2af8e87857
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-02-27 23:38:31 +0600

    27 Feb 2023

---
+++ Codeforces/A_Recent_Actions.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0)
+#define TxtIO  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define f2(i, n) for (int i = 1; i < n; i++)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define int long long
+#define INF 1000000000
+#define mod 1000000007
+int Ceil(int a, int b){return (a + b - 1) / b;}
+//_________________template______________
+
+template <typename T> // printByVectorName
+ostream& operator<<(ostream &os, const vector<T> &v) {for (auto e : v){os << e << " ";}return os;}
+
+void sol()
+{
+    int n,m;cin>>n>>m;
+    vector<int>v(m);
+    map<int,int>map;
+    f0(i,m){
+        cin>>v[i];
+    }
+
+    int arr[n+1];
+    for(int i = 1; i<= n; i++){
+        arr[i] = -1;
+    }    
+    int idx = n;
+    int time = 1;
+    for(int i = 0; i<m; i++){
+        if(map[v[i]] == 0){
+            arr[idx] = time;
+            idx--;
+        }
+        map[v[i]]++;
+        time++;
+    }
+    for(int i = 1; i<= n; i++){
+        cout << arr[i]<<" ";
+    }cout<<endl;
+}
+//Before Submit handle the case for 0 and 1
+int32_t main()
+{
+    FastIO;
+    //TxtIO;
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        sol();
+    }
+}
