6f2c0b4dc719fc89c8d74792416564eb9d9096db
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-07-24 22:45:31 +0600

    24 july 2023

---
+++ Codeforces/D_Flipper.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0)
+#define TxtIO  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define f2(i, n) for (int i = 1; i < n; i++)
+#define endl "\n"
+#define pb push_back
+//#define mp make_pair
+#define vi vector<int>
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define sqrt sqrtl
+#define ff first
+#define ss second
+#define int long long
+#define INF 1000000000
+#define mod 1000000007
+//_________________Function_______________
+int nPr(int n, int r){ if (r > n / 2) r = n - r; int result = 1; for (int i = 1; i <= r; ++i) { result *= n - i + 1;} return result; }
+int nCr(int n, int r){ if (r > n / 2) r = n - r; int result = 1; for (int i = 1; i <= r; ++i) { result *= n - i + 1; result /= i; } return result; }
+int Ceil(int a, int b){return (a + b - 1) / b;}
+int pow(int a, int b){ int res = 1; while (b){ if(b&1){ res *= a; b--;} a *= a; b /= 2; } return res;}
+//_________________template______________
+
+template<typename T> istream& operator >> (istream &istream, vector<T> &v) {for (auto &it : v) cin >> it; return istream;}
+template <typename T> ostream& operator<<(ostream &os, const vector<T> &v) {for (auto e : v){os << e << " ";}return os;}
+
+void sol()
+{
+    int n,mx=0,indx=1; cin>>n;
+    int a[n];
+ 
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+        if(a[i]>mx && i>0){
+           mx=a[i]; indx=i;
+        }
+    }
+    if(n==1){
+        cout<<a[0]<<"\n"; return;
+    }
+    for(int i=indx; i<n; i++)cout<<a[i]<<" ";
+    if(indx!=n-1){
+        cout<<a[indx-1]<<" "; indx-=2;
+    }
+    else indx--;
+    for(int i=indx; i>0; i--){
+        if(a[i]>=a[0])cout<<a[i]<<" ";
+        else{
+             break;
+        }
+        indx=i-1;
+    }
+    for(int i=0; i<=indx; i++)cout<<a[i]<<" ";
+    cout<<"\n";
+}
+//Before Submit handle the case for 0 and 1
+int32_t main()
+{
+    FastIO;
+    //TxtIO;
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        sol();
+    }
+}
