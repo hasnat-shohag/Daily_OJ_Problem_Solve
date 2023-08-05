987d8a7ff954eab96b28c829c1a4d546f929cea5
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-08-05 14:25:53 +0600

    5 augusy 2023

---
+++ C_C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0)
+#define TxtIO  freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
+#define f0(i, n) for (int i = 0; i < n; i++)
+#define f1(i, n) for (int i = 1; i <= n; i++)
+#define f2(i, n) for (int i = 1; i < n; i++)
+#define endl "\n"
+#define pb push_back
+//#define mp make_pair
+#define vi vector<int>
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define sqrt sqrtl
+#define ff first
+#define ss second
+#define int long long
+#define INF 1000000000
+#define mod 1000000007
+//_________________Function_______________
+int nPr(int n, int r){ if (r > n / 2) r = n - r; int result = 1; for (int i = 1; i <= r; ++i) { result *= n - i + 1;} return result; }
+int nCr(int n, int r){ if (r > n / 2) r = n - r; int result = 1; for (int i = 1; i <= r; ++i) { result *= n - i + 1; result /= i; } return result; }
+int Ceil(int a, int b){return (a + b - 1) / b;}
+int pow(int a, int b){ int res = 1; while (b){ if(b&1){ res *= a; b--;} a *= a; b /= 2; } return res;}
+//_________________template______________
+
+template<typename T> istream& operator >> (istream &istream, vector<T> &v) {for (auto &it : v) cin >> it; return istream;}
+template <typename T> ostream& operator<<(ostream &os, const vector<T> &v) {for (auto e : v){os << e << " ";}return os;}
+
+void sol()
+{
+    int n;cin>>n;
+    string grid[n];
+
+    f0(i,n){
+        cin>>grid[i];
+    }
+    for(int i = 0; i<n;i++){
+        sort(all(grid[i]));
+    }
+    bool flag = true;
+    // for(int i = 0; i<n-1; i++){
+    //     if(grid[i] > grid[i+1]){
+    //         flag = false;
+    //         break;
+    //     }
+    // }
+    // if(flag){
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n-1; j++){
+                if(grid[j][i] > grid[j+1][i]){
+                    flag = false;
+                    break;
+                }
+            }
+        }
+    // }
+    if(flag) cout <<"YES"<<endl;
+    else cout <<"NO" << endl;
+}
+//Before Submit handle the case for 0 and 1
+int32_t main()
+{
+    FastIO;
+    //TxtIO;
+    int tt;
+    tt = 1;
+    cin >> tt;
+    while (tt--)
+    {
+        sol();
+    }
+}
