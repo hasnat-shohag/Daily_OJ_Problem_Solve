4cf2f503d7c669f7efcc9ed8d7ffeac4b8d69aba
hasnat-shohag <hasnat.ru.ice19@gmail.com>
2023-09-30 00:34:22 +0600

    cf prime-factorization problem

---
+++ Codeforces/C_Prime_factorization.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define FastIO ios_base::sync_with_stdio(0), cin.tie(0)
+#define f0(i,n)  for(int i = 0; i<n; i++)
+#define f1(i,n)  for(int i = 1; i<=n; i++)
+#define f2(i,n)  for(int i = 1; i<n; i++)
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define vi vector<int>
+#define pi pair<int, int>
+#define all(x) x.begin(), x.end()
+#define f first
+#define sc second
+#define int long long
+#define INF 1000000000
+int Ceil(int a, int b) { return a/b+(a%b != 0); }
+
+vector<int>low(1e6); 
+void sieve(){
+    int n = 1e6;
+    for(int i = 2; i<=n; i++){
+        low[i] = i;
+    }
+
+    for(int i = 2; i*i <= n; i++){
+        if(low[i] == i){
+            for(int j =  i*i; j<= n; j += i){
+                if(low[j] == j){ 
+                    low[j] = i;
+                }
+            }
+        }
+    }
+}
+void sol()
+{
+    int n;
+    cin>>n;
+
+    vector<int>res;
+
+    while (n>1) 
+    {
+        res.pb(low[n]);
+        n /= low[n];
+    }
+    cout << res[0];
+    for(int i = 1; i<res.size(); i++){
+        cout << "*"<<res[i];
+    }
+    cout << endl;
+}
+
+int32_t main(){
+    FastIO;
+    sieve();
+    //TxtIO;
+    int tt;
+    tt = 1;
+    // cin >> tt;
+    while (tt--)
+    {
+        sol();
+    }
+}
